702159ae17a364a8e64633fa11f414e5a6dc86b1
AyaMahmoud <aya_conan_1992@yahoo.com>
2017-12-14 14:37:26 +0200

    write comment

--- app/src/main/java/com/av/ibnammy/MainActivity.java
+++ app/src/main/java/com/av/ibnammy/MainActivity.java
@@ -9,7 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Edit by Aya
+        // Edit by Aya
+        // 12/14/2014
 
     }
 }
